979325626584f4a792433a174d30a077e2934a8c
Isma <2023_Ismael.younes@iticbcn.cat>
2024-12-16 08:29:12 +0100

    clases creades

---
+++ 01-Fils/Fils.java
@@ -0,0 +1,11 @@
+public class Fils extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    public Fils(String id) {
+        super(id);
+    }
+}
---
+++ 01-Fils/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+        Fils fil = new Fils("PEPE");
+        Fils fil2 = new Fils("JUAN");
+        System.out.println(fil.toString());
+    }
+}
